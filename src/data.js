c74f9fe06281d74a6a9beb36466f0fb063d61b15
Kamil Staszewski <kamilstaszewski95@gmail.com>
2018-11-26 18:43:46 +0100

    added prop-types and antd

---
+++ src/data.js
@@ -0,0 +1,221 @@
+const data = {
+  data: {
+    1: {
+      id: 1,
+      name: "Bitcoin",
+      symbol: "BTC",
+      website_slug: "bitcoin",
+      rank: 1,
+      circulating_supply: 17393400,
+      total_supply: 17393400,
+      max_supply: 21000000,
+      quotes: {
+        USD: {
+          price: 3863.73632983,
+          volume_24h: 6377241392.86934,
+          market_cap: 67203511479,
+          percent_change_1h: -0.58,
+          percent_change_24h: -10.23,
+          percent_change_7d: -31.06
+        }
+      },
+      last_updated: 1543158141
+    },
+    2: {
+      id: 2,
+      name: "Litecoin",
+      symbol: "LTC",
+      website_slug: "litecoin",
+      rank: 8,
+      circulating_supply: 59304863,
+      total_supply: 59304863,
+      max_supply: 84000000,
+      quotes: {
+        USD: {
+          price: 29.184154199,
+          volume_24h: 532213586.645347,
+          market_cap: 1730762271,
+          percent_change_1h: -0.04,
+          percent_change_24h: -8.59,
+          percent_change_7d: -31.27
+        }
+      },
+      last_updated: 1543158123
+    },
+    52: {
+      id: 52,
+      name: "XRP",
+      symbol: "XRP",
+      website_slug: "ripple",
+      rank: 2,
+      circulating_supply: 40327341704,
+      total_supply: 99991780039,
+      max_supply: 100000000000,
+      quotes: {
+        USD: {
+          price: 0.3521430539,
+          volume_24h: 1172115317.56767,
+          market_cap: 14200993264,
+          percent_change_1h: -0.72,
+          percent_change_24h: -12.67,
+          percent_change_7d: -31.23
+        }
+      },
+      last_updated: 1543158122
+    },
+    328: {
+      id: 328,
+      name: "Monero",
+      symbol: "XMR",
+      website_slug: "monero",
+      rank: 9,
+      circulating_supply: 16597126,
+      total_supply: 16597126,
+      max_supply: null,
+      quotes: {
+        USD: {
+          price: 56.8360122079,
+          volume_24h: 21898261.713193,
+          market_cap: 943314453,
+          percent_change_1h: -1.02,
+          percent_change_24h: -12.79,
+          percent_change_7d: -36.25
+        }
+      },
+      last_updated: 1543158123
+    },
+    512: {
+      id: 512,
+      name: "Stellar",
+      symbol: "XLM",
+      website_slug: "stellar",
+      rank: 6,
+      circulating_supply: 19149937395,
+      total_supply: 104483100707,
+      max_supply: null,
+      quotes: {
+        USD: {
+          price: 0.14503671,
+          volume_24h: 135650301.596401,
+          market_cap: 2777443917,
+          percent_change_1h: -1.01,
+          percent_change_24h: -18.17,
+          percent_change_7d: -42.29
+        }
+      },
+      last_updated: 1543158124
+    },
+    825: {
+      id: 825,
+      name: "Tether",
+      symbol: "USDT",
+      website_slug: "tether",
+      rank: 7,
+      circulating_supply: 1806421736,
+      total_supply: 2580109502,
+      max_supply: null,
+      quotes: {
+        USD: {
+          price: 0.9799558319,
+          volume_24h: 4923022999.1521,
+          market_cap: 1770213515,
+          percent_change_1h: -0.11,
+          percent_change_24h: -0.37,
+          percent_change_7d: -0.99
+        }
+      },
+      last_updated: 1543158128
+    },
+    1027: {
+      id: 1027,
+      name: "Ethereum",
+      symbol: "ETH",
+      website_slug: "ethereum",
+      rank: 3,
+      circulating_supply: 103417981,
+      total_supply: 103417981,
+      max_supply: null,
+      quotes: {
+        USD: {
+          price: 111.632877992,
+          volume_24h: 2314205800.894,
+          market_cap: 11544846852,
+          percent_change_1h: 0.1,
+          percent_change_24h: -8.89,
+          percent_change_7d: -36.63
+        }
+      },
+      last_updated: 1543158136
+    },
+    1765: {
+      id: 1765,
+      name: "EOS",
+      symbol: "EOS",
+      website_slug: "eos",
+      rank: 5,
+      circulating_supply: 906245118,
+      total_supply: 1006245120,
+      max_supply: null,
+      quotes: {
+        USD: {
+          price: 3.2794132487,
+          volume_24h: 1077172553.12441,
+          market_cap: 2971952245,
+          percent_change_1h: -0.63,
+          percent_change_24h: -8.23,
+          percent_change_7d: -28.53
+        }
+      },
+      last_updated: 1543158131
+    },
+    1831: {
+      id: 1831,
+      name: "Bitcoin Cash",
+      symbol: "BCH",
+      website_slug: "bitcoin-cash",
+      rank: 4,
+      circulating_supply: 17476400,
+      total_supply: 17476400,
+      max_supply: 21000000,
+      quotes: {
+        USD: {
+          price: 173.512732675,
+          volume_24h: 128362677.945921,
+          market_cap: 3032377921,
+          percent_change_1h: -0.8,
+          percent_change_24h: -15.89,
+          percent_change_7d: -54.7
+        }
+      },
+      last_updated: 1543158132
+    },
+    2010: {
+      id: 2010,
+      name: "Cardano",
+      symbol: "ADA",
+      website_slug: "cardano",
+      rank: 10,
+      circulating_supply: 25927070538,
+      total_supply: 31112483745,
+      max_supply: 45000000000,
+      quotes: {
+        USD: {
+          price: 0.0358515137,
+          volume_24h: 42493328.4917455,
+          market_cap: 929524724,
+          percent_change_1h: -1.2,
+          percent_change_24h: -15.31,
+          percent_change_7d: -41.94
+        }
+      },
+      last_updated: 1543158136
+    }
+  },
+  metadata: {
+    timestamp: 1543157376,
+    num_cryptocurrencies: 2071,
+    error: null
+  }
+}
+
+module.exprots = data;
